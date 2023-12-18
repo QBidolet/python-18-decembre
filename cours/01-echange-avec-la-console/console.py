d1b64aeb0cd49bf494a72b91c6a5ec1c60983a15
QBidolet <quentin.bidolet@gmail.com>
2023-12-18 13:47:47 +0100

    comment console

---
+++ cours/01-echange-avec-la-console/console.py
@@ -0,0 +1,6 @@
+# Quelques commandes python
+# python --version
+# pip list
+# python (pour rentrer dans l'interpreteur)
+# import this
+# exit()
---
+++ cours/02-premier-programme/01-comment.py
@@ -0,0 +1,14 @@
+# mon commentaire
+# autre ligne
+# une autre ligne
+
+
+"""
+ Un commentaire multiligne ?
+ C'est une chaine de caractère.
+ DOCSTRING
+ A placer en haut d'un fichier pour le décrire.
+ En haut d'une fonction pour la décrire.
+ En haut d'une classe pour la décrire.
+"""
+
