3861b56ee37a44b89584bd3c949ba355ff565d6a
QBidolet <quentin.bidolet@gmail.com>
2023-12-18 15:10:08 +0100

    maths

---
+++ cours/03-types-base/02-maths.py
@@ -0,0 +1,17 @@
+# Modulo
+# 9 / 3 = 3
+# 10 / 3 = 3.333...
+# 9 % 3 = 0
+# 10 % 3 = 1
+# nombre % 2 == 0 pour vérifier si un nombre pair
+print(10 % 3)
+
+print(2 + 2)
+print(2 - 2)
+print(10 / 3)
+print(10 // 3)
+print(2 * 2)
+print(2 ** 3)
+
+# Priorité : les mêmes qu'en math classique.
+print((1 + 1) ** (5 - 2))
