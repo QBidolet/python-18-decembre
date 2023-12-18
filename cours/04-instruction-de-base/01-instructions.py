d5249033b10eb800f0f1157ee3712b92b58b21ad
QBidolet <quentin.bidolet@gmail.com>
2023-12-18 17:28:41 +0100

    karam

---
+++ cours/04-instruction-de-base/01-instructions.py
@@ -0,0 +1,16 @@
+nombre_1 = 25
+nombre_2 = 50
+
+# condition
+if nombre_1 > nombre_2:
+    print("Nombre_1 est plus grand.")
+elif nombre_1 == nombre_2:
+    print("Nombre1 et nombre2 sont égaux.")
+else:
+    print("Nombre 2 est supérieur à nombre 1.")
+
+# boucle while
+i = 0
+while i < 6:
+    print(i)
+    i += 1
---
+++ exercices/exercice_1.py
@@ -0,0 +1,11 @@
+"""
+Vous devez écrire un programme qui demande à l'utilisateur de saisir une vitesse en miles/heure.
+ et vous devez afficher cette vitesse en km/h et m/s.
+
+BONUS :
+Vous devez réinviter votre utilisateur à saisir une valeur une fois
+la conversion terminé et boucler de cette manière.
+
+Indication pour la conversion : pour passer des miles/heure au mètre par seconde il faut diviser par 2.237
+Indication pour la conversion : pour passer des miles/heure au km par heure il faut multiplier par 1.609
+"""
